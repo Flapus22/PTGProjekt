2e4c70ba87afc93d483367ced90315e027101ee0
Adriangrz <adriang1412g@gmail.com>
2022-01-08 20:33:45 +0100

    Dodanie do przeciwnika funkcji patrolowania

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,22 +8,34 @@ public class Enemy : MonoBehaviour
     private bool isPlayerAlive;
     private float dist;
     private bool isHit = false;
-    public float cooldown = 1f;
     private float lastAttackedAt;
+    private float waitTime;
+    private int randomSpot;
+
+    public float cooldown = 1f;
     public float moveSpeed;
     public float howClose;
-    // Start is called before the first frame update
+    public float startWaitTime;
+
+    public Transform[] moveSpots;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         isPlayerAlive = player.GetComponent<NewPlayerController>().isAlive;
         dist = Vector3.Distance(player.position, transform.position);
-        if(dist <= howClose && isPlayerAlive)
+        if (!(dist <= 1f))
+        {
+            Patrol();
+        }
+        if (dist <= howClose && isPlayerAlive)
         {
             transform.LookAt(player);
             GetComponent<CharacterController>().Move(transform.forward * moveSpeed * Time.deltaTime);
@@ -39,6 +51,24 @@ public class Enemy : MonoBehaviour
         {
             lastAttackedAt = 0.0f;
             isHit = false;
+        }
+    }
+
+    void Patrol()
+    {
+        transform.LookAt(moveSpots[randomSpot].transform);
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, moveSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if(waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
         }
     }
 }
