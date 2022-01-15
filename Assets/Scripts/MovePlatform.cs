5a6cb48f162480923a31f3db0ff87b88d5a39e82
Flapus22 <72880003+Flapus22@users.noreply.github.com>
2022-01-15 19:02:15 +0100

    dodanie ruchomej platformy

---
+++ Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    public Vector3 delta;
+    Vector3 startPosition;
+    Rigidbody rigidbody;
+
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float velocity = 50f / delta.sqrMagnitude;
+        float change = (Mathf.Sin(Time.timeSinceLevelLoad*velocity) + 1f) / 2f;
+        rigidbody.position = Vector3.Lerp(startPosition, startPosition + delta, change);
+    }
+
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.gray;
+
+        if(Selection.activeTransform==transform)
+        {
+            Gizmos.color = Color.yellow;
+        }
+
+        Vector3 ghostPosition = transform.position + delta;
+        Vector3 ghostSize = transform.rotation * transform.localScale * 2f;
+
+        Gizmos.DrawWireCube(ghostPosition, ghostSize);
+    }
+#endif
+}
