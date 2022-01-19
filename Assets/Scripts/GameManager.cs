51ab24162b31380ed9afa267cd882378a1c34c85
Debenter <danielsenecki@o2.pl>
2022-01-19 11:20:02 +0100

    komentarz kodu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,55 +7,58 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
+    //public static GameManager Instance;
 
-    public GameState State;
+    //public GameState State;
 
-    public static event Action<GameState> OnGameStateChanged;
-    private void Awake()
-    {
-        Instance = this;
-    }
+    //public static event Action<GameState> OnGameStateChanged;
+    //private void Awake()
+    //{
+    //    Instance = this;
+    //}
 
-    private void Start()
-    {
-        UpdateGameState(GameState.GamePlay);
-    }
+    //private void Start()
+    //{
+    //    UpdateGameState(GameState.GamePlay);
+    //}
 
-    public void UpdateGameState(GameState newState)
-    {
-        State = newState;
+    //public void UpdateGameState(GameState newState)
+    //{
+    //    State = newState;
 
-        switch (newState)
-        {
-            case GameState.GamePlay:
-                HandleGamePlay();
-                break;
-            case GameState.GameOver:
-                HandleGameOver();
-                break;
-            case GameState.Victory:
-                HandleVictory();
-                break;
-        }
+    //    switch (newState)
+    //    {
+    //        case GameState.GamePlay:
+    //            HandleGamePlay();
+    //            break;
+    //        case GameState.GameOver:
+    //            HandleGameOver();
+    //            break;
+    //        case GameState.Victory:
+    //            HandleVictory();
+    //            break;
+    //    }
 
-        OnGameStateChanged?.Invoke(newState);
-    }
+    //    OnGameStateChanged?.Invoke(newState);
+    //}
 
-    private void HandleGamePlay()
-    {
-        
-    }
+    //private void HandleGamePlay()
+    //{
 
-    private void HandleGameOver()
-    {
+    //}
 
-    }
+    //private void HandleGameOver()
+    //{
+    //    if (!GetComponent<NewPlayerController>().isAlive)
+    //    {
 
-    private void HandleVictory()
-    {
+    //    }
+    //}
 
-    }
+    //private void HandleVictory()
+    //{
+
+    //}
 
     public void ReturnButton()
     {
@@ -63,9 +66,9 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(sceneID);
     }
 }
-public enum GameState
-{
-    GamePlay,
-    GameOver,
-    Victory
-}
+//public enum GameState
+//{
+//    GamePlay,
+//    GameOver,
+//    Victory
+//}
